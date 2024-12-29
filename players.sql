72350fbde2e96a5f2f91832a080433147ea3c210
carson-troutman <carson.troutman@gmail.com>
2024-12-29 16:46:25 -0500

    updated requirements.txt and added sql file

---
+++ players.sql
@@ -0,0 +1,28 @@
+-- Use sql queries to find the best fpl picks 
+-- based on the data from the 24-25 season so far
+
+-- Data points to consider:
+-- cost (now_cost)
+-- position
+-- team
+-- total_points
+-- assists
+-- expected_assists_per_90
+-- goals (goals_scored)
+-- expected_goals_per_90
+-- expected_goal_involvments_per_90
+-- creativity
+-- creativity_rank
+-- influence_rank
+-- threat
+-- clean_sheets_per_90
+-- clean_sheets
+-- expected_goals_conceded_per_90
+-- saves
+-- saves_per_90
+-- minutes
+-- starts
+-- starts_per_90
+-- bonus (bps)
+--status (a - available, u - unavailable)
+
